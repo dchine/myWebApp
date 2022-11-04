476ad2df86890da96ab3fe023295b9e9408b1e23
Daniel Hine <dchine@outlook.com>
2022-11-04 14:46:31 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "This is the index."
+
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
